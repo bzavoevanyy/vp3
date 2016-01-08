e6d4061111192803499ca06c8da7dc6a1a61c33c
BovdyrSergiy <bovdirserg@gmail.com>
2016-01-08 17:30:18 +0200

    Error function

--- - dev/scripts/uploudImages.js
+++ - dev/scripts/uploudImages.js
@@ -8,7 +8,41 @@ var uploudImages = (function () {
 
 		// прослушка событий...
 	var _setUpListners = function () {
-		
+
+		var sendData = true,
+				formWithFile = $("#form-add-files");
+				
+		$('#source-file').fileupload({
+			url: 'add-project.php',
+			type: 'POST',
+			dataType : 'json',
+			autoUpload : false,
+			add : function(e, data){
+				formWithFile.on("submit",function(){
+					if (formWithFile.valid()) {
+						if(sendData){
+							data.formData = formWithFile.serializeArray();
+							sendData = false;
+						}
+						data.submit();
+					} else {
+						console.log('Не прошел валидацию!');
+					}
+				});
+			},
+			done: function(e, data){
+				sendData = true;
+				if (data.result.status == 'success') {
+					setTimeout("location.reload();", 1000);
+				} else {
+					console.log(data.result.text);
+				}
+			},
+			fail: function (e, data) {
+				console.log('error');
+			}
+		});
+
 	}
 
 	return {
