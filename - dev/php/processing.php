a195505862cde0f65242de9bbdb7e9c6b37c5025
Pavel <semchenko.pavel@gmail.com>
2016-01-17 23:01:20 +0300

    Processing loaded files. RC1

--- - dev/php/processing.php
+++ - dev/php/processing.php
@@ -1,15 +1,32 @@
 <?php
-$URL_image = '';
-$URL_image_watermark = '';
-$positionX = '150';
-$positionY = '100';
-$opacity = '50';
 
 require_once "../../vendor/autoload.php";
 use \WideImage\WideImage as WideImage;
 
-$image = WideImage::LoadFromFile($URL_image);
-$image_watermark = WideImage::LoadFromFile($URL_image_watermark);
+$sourceImage = $_POST['sourceImage'];
+$watermark = $_POST['watermark'];
+$sourceK = $_POST['sourceK'];
+$watermarkK = $_POST['watermarkK'];
+$positionX = $_POST['x']*$sourceK;
+$positionY = $_POST['y']*$sourceK;
+$opacity = $_POST['opacity'];
+
+$image = WideImage::LoadFromFile("uploads/$sourceImage");
+$image_watermark = WideImage::LoadFromFile("uploads/$watermark");
+
+if ($watermarkK > 1) {
+    $imgInfo = getimagesize("uploads/$watermark");
+    $newWidth = $imgInfo[0]/$watermarkK;
+    $newHeight = $imgInfo[1]/$watermarkK;
+    $image_watermark_resized = $image_watermark->resize($newWidth, $newHeight);
+    $image_watermark_resized->saveToFile('uploads/watermarkResized.jpg');
+    $image_watermark = WideImage::LoadFromFile('uploads/watermarkResized.jpg');
+}
 
 $image_merged = $image->merge($image_watermark, $positionX, $positionY, $opacity);
-$image_merged->output('jpg');
+$image_merged->saveToFile('uploads/result.jpg');
+
+$data = $_SERVER['HTTP_HOST'].'/server/uploads/result.jpg';
+
+echo json_encode($data);
+exit;
